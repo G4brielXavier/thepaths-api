6aaedafbc7deb1008a6a16031845471c46a8ecbe
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2025-05-08 13:49:37 -0400

    improvements in docker and deployer

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import languages
+
+origins = [
+    "http://localhost:5173",
+    "https://thepaths.space"
+]
 
 app = FastAPI(
     title="The Paths",
     description="An Api created to The Paths.",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 app.include_router(languages.router, prefix="/languages", tags=["languages"])
 
